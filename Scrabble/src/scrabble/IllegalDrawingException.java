835a9ff75df8c40ca1c0610e2166b485bbcf5345
Bruno Mermet <Bruno.Mermet@univ-lehavre.fr>
2016-06-28 01:44:28 +0200

    Initial state

---
+++ Scrabble/src/scrabble/IllegalDrawingException.java
@@ -0,0 +1,15 @@
+package scrabble;
+
+/**
+ * Such an exception is thrown when a tile is indicated to be drawn whereas
+ * no such tile remains in the bag.
+ * @author mermet
+ *
+ */
+public class IllegalDrawingException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+
+  public IllegalDrawingException(char c) {
+    super("Invalid input. More " + c + "'s have been taken from the bag than possible");
+  }
+}
