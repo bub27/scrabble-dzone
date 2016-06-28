b319fada8f51f779cdae32a2b09638334784a1b3
Bruno Mermet <Bruno.Mermet@univ-lehavre.fr>
2016-06-28 09:12:10 +0200

    Javadoc comment added to NonExistingLetterException

--- scrabble/src/main/java/scrabble/NonExistingLetterException.java
+++ scrabble/src/main/java/scrabble/NonExistingLetterException.java
@@ -1,5 +1,10 @@
 package scrabble;
 
+/**
+ * This kind of exception is thrown when an illegal character is given.
+ * @author mermet
+ *
+ */
 public class NonExistingLetterException extends ScrabbleException {
   public NonExistingLetterException(char c) {
     super(c + " is not a valid letter");
